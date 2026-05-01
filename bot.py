0c2393c38cb3972ef216cd9ff70885e43dcf8067
rt0323 <137693743+rt0323@users.noreply.github.com>
2026-05-02 04:07:38 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,6 +92,7 @@ async def schedule(message: Message):
 # -------------------------
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
