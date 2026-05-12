0c8a7b1ec6e4657ead9e84c889e15871143e2b1d
rt0323 <137693743+rt0323@users.noreply.github.com>
2026-05-12 23:17:56 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -698,10 +698,9 @@ async def cmd_resetbets(message: Message):
 # 🔥 СТАРТ
 # -------------------------
 
- 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
- 
+
 if __name__ == "__main__":
     asyncio.run(main())
